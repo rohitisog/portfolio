193d9def7c0a6b008405966fd658ef67894f3514
rohitisog <rohitisog@gmail.com>
2025-01-31 22:13:03 +0530

    fixed extra sapce

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,12 +16,12 @@ const Home = () => {
           </p>
           <div className="flex justify-center ">
             <button className=" bg-gradient-to-r from-orange-500 to-orange-800 hover:border py-2 px-3 mx-4 rounded cursor-pointer w-32 h-10">
-              {" "}
-              About{" "}
+            
+              About
             </button>
             <button className=" bg-gradient-to-r from-orange-500 to-orange-800 hover:border mx-4 py-2 px-3 rounded cursor-pointer w-32 h-10">
-              {" "}
-              Get In Touch{" "}
+            
+              Get In Touch
             </button>
           </div>
 
@@ -29,7 +29,7 @@ const Home = () => {
         <div className="w-full lg:w-1/2 flex flex-wrap  justify-between gap-2 px-8">
           <div className=" p-4 border-none  bg-neutral-700 rounded-2xl ">
             <h2 className="text-3xl bg-gradient-to-r from-orange-500 to-orange-800 bg-clip-text text-transparent">
-              {" "}
+            
               Professional Journey
             </h2>
             <p>
