0e02d98ae600814d7becf91538a23694d736b8d2
rohitisog <rohitisog@gmail.com>
2025-02-13 19:26:25 +0530

    reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
-import React from "react";
-import { Routes, Route } from "react-router";
-import About from "./components/About";
-import Blogs from "./components/Blogs";
-import Projects from "./components/Projects";
-import Work from "./components/Work";
-import GetInTouch from "./components/GetInTouch";
-import Home from "./components/Home";
-import Header from "./components/Header";
+import React from 'react'
 
 const App = () => {
   return (
     <>
-    <Header/>
-      <Routes>
-        <Route index path="/" element={<Home />} />
-        <Route  path="/about" element={<About />} />
-        <Route path="/work" element={<Work />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/getInTouch" element={<GetInTouch />} />
-        {/*  twitter , linkedin, github , read cv */}
-      </Routes>
+    <div>
+      
+    </div>
+    
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
