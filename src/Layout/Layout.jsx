db122411dccf904b7eaed53857db94c135f1f189
rohitisog <rohitisog@gmail.com>
2025-02-14 19:15:48 +0530

    fixed bg color

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -4,7 +4,7 @@ import Navbar from "../components/Navbar.jsx";
 
 const Layout = () => {
   return (
-    <div className="w-full max-w-4xl mx-auto md:max-w-3xl md:bg-blue-300 lg:bg-yellow-400">
+    <div className="w-full max-w-4xl mx-auto md:max-w-3xl">
       <Navbar />
       <Outlet /> 
     </div>
