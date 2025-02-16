e28882e812d5fefa4ecdc7d63c7087b32f242c69
rohitisog <rohitisog@gmail.com>
2025-02-16 21:58:20 +0530

    fix ac

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -6,6 +6,8 @@ const Achievements = () => {
     <div>
       <h1>My Achievements</h1>
       <ul>
+        <li>Won Inter School Hackathon</li>
+        <li>Won ETHGlobal Online 2023 Hackathon</li>
         <li>Graduated from the GNDU University</li>
         <li>Completed a course in from Angela Yu</li>
       </ul>
