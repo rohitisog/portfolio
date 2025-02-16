7b7b71c0dc3f7cb71698e7ac864f193356da3a91
rohitisog <rohitisog@gmail.com>
2025-02-16 21:57:21 +0530

    working achivement

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Achievements = () => {
   return (
-    <div>Achievements</div>
+    <>
+    <div>
+      <h1>My Achievements</h1>
+      <ul>
+        <li>Graduated from the GNDU University</li>
+        <li>Completed a course in from Angela Yu</li>
+      </ul>
+    </div>
+    </>
   )
 }
 
